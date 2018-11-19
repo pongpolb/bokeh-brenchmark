a1923a5b7cd2d273bdf04f336255d35c18f8e1b8
Pongpol Hussein <pongpol.bruno@gmail.com>
2018-11-19 21:51:51 +0700

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import faker from 'faker';
 import './App.css';
 
 class App extends Component {
+  state = {
+    number: 0,
+    isFetching: false,
+  }
+
+  onChange = (event) => {
+    this.reRenderPieGraph(event.target.value)
+  }
+
+  reRenderPieGraph = (number) => {
+    const pie_data = {
+      labels: [],
+      values: [],
+    }
+
+    faker.seed(123)
+    for (let i = 0; i < number; i++) {
+      pie_data.labels.push(faker.random.uuid())
+      pie_data.values.push(faker.random.number())
+    }
+
+
+    console.log(`Bokeh ${window.Bokeh.version}`)
+    window.Bokeh.set_log_level("info")
+
+
+    const p11 = window.Bokeh.Charts.pie(pie_data)
+
+    const doc = new window.Bokeh.Document()
+    doc.add_root(p11)
+
+    const div = document.getElementById("plot")
+
+    div.innerHTML = ""
+
+    window.Bokeh.embed.add_document_standalone(doc, div)
+  }
+
+  reRenderBarGraph = (number) => {
+    const bar_data = [
+      ['AAAAA'],
+      ['BBBBB'],
+    ]
+
+    faker.seed(123)
+    for (let i = 0; i < number; i++) {
+      bar_data[0].push(faker.random.uuid())
+      bar_data[1].push(faker.random.number())
+    }
+
+
+    console.log(`Bokeh ${window.Bokeh.version}`)
+    window.Bokeh.set_log_level("info")
+
+    const p21 = window.Bokeh.Charts.bar(bar_data, {axis_number_format: "0.[00]a"})
+
+    const doc = new window.Bokeh.Document()
+    doc.add_root(p21)
+
+    const div = document.getElementById("plot")
+
+    div.innerHTML = ""
+
+    window.Bokeh.embed.add_document_standalone(doc, div)
+  }
+
   render() {
+    const { number, isFetching } = this.props
+    if (isFetching) {
+      return (
+        <div>
+          isLoading
+        </div>
+      )
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Input Number: <input type="number" onChange={this.onChange} defaultValue={ number } />
+        <div id="plot" />
       </div>
     );
   }
